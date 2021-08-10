0abbfa0b3929f2e9ef8cd3a038550e38e6e5eda1
hhui64 <907322015@qq.com>
2021-08-10 20:18:19 +0800

    chore(deps): add `ant-design-vue`

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(antd).mount('#app')
