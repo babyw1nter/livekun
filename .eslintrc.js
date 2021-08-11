c5c0ad2a6e8619b1353f6a677dd1944d20ec7d5e
hhui64 <907322015@qq.com>
2021-08-11 11:40:09 +0800

    workflow: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+    'space-before-function-paren': 'off'
   },
   overrides: [
     {
