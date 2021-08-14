a9fd2e022a5bb976bcaca1313b8c1583d537c384
hhui64 <907322015@qq.com>
2021-08-15 03:33:51 +0800

    workflow(eslint): set `no-unused-vars` to off

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
     'space-before-function-paren': 'off'
   },
   overrides: [
