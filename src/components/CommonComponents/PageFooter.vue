4e2f763b80a3a9042ea04d8267ee88634e2e55cb
hhui64 <907322015@qq.com>
2021-08-23 00:04:34 +0800

    perf(PageFooter): 优化页脚组件样式

--- src/components/CommonComponents/PageFooter.vue
+++ src/components/CommonComponents/PageFooter.vue
@@ -1,4 +1,19 @@
 <template>
-  <a-divider />
-  <p style="text-align: center; color: #ccc;">© 2021 hhui64. Powered by CCLinkJS and Designed for NaiLing.</p>
+  <footer>
+    <a-divider />
+    <p class="copyright">
+      © 2021 hhui64. Powered by CCLinkJS and Designed for NaiLing.
+    </p>
+  </footer>
 </template>
+
+<style lang="less">
+footer {
+  border: 1px solid transparent;
+  .copyright {
+    text-align: center;
+    color: #ccc;
+    margin-bottom: 24px;
+  }
+}
+</style>
