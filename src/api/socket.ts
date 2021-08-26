c19f2af675bd68f4b1cd611d3fcb6270795229c1
hhui64 <907322015@qq.com>
2021-08-26 11:05:27 +0800

    feat(api/socket): 新增根据 UUID 创建指定协议的 WS 实例

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -1,17 +1,25 @@
 import store from '@/store'
+import router from '@/router'
 import { baseWsURL } from './http'
 
 export const createSocket = (
   onMessageCallback: (ev: MessageEvent, websocket: WebSocket) => void,
   protocols: 'gift-capsule' | 'chat-message' | 'gift-card' | string
-): WebSocket => {
+): WebSocket | null => {
+  const uuid = router.currentRoute.value.query.uuid || ''
+
+  if (!uuid) {
+    console.warn('没有 UUID 参数，不连接 socket 服务器！')
+    return null
+  }
+
   console.log(`[${protocols}]`, '正在创建 WS 连接...')
-  const websocket = new WebSocket(baseWsURL, protocols)
+  const websocket = new WebSocket(baseWsURL, protocols + '-' + uuid)
 
   websocket.addEventListener('open', () => {
     console.log(`[${protocols}]`, '连接成功!')
     store.dispatch('getRemoteConfig')
-    store.dispatch('getRemoteStatus')
+    // store.dispatch('getRemoteStatus')
   })
   websocket.addEventListener('error', () => {
     console.error(`[${protocols}]`, '连接错误!')
