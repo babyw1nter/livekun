37b61696a528e16d7d3497a0f9d4e314172ec8c8
hhui64 <907322015@qq.com>
2021-08-27 05:38:36 +0800

    perf(api/socket): 优化 WS 连接重连逻辑

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -17,16 +17,22 @@ export const createSocket = (
   const websocket = new WebSocket(baseWsURL, protocols + '-' + uuid)
 
   websocket.addEventListener('open', () => {
-    console.log(`[${protocols}]`, '连接成功!')
+    console.log(`[${protocols}]`, '连接成功！')
     store.dispatch('getRemoteConfig')
-    // store.dispatch('getRemoteStatus')
   })
   websocket.addEventListener('error', () => {
-    console.error(`[${protocols}]`, '连接错误!')
+    console.error(`[${protocols}]`, '连接错误！')
   })
-  websocket.addEventListener('close', () => {
-    console.warn(`[${protocols}]`, '连接关闭!')
+  websocket.addEventListener('close', ev => {
+    console.warn(`[${protocols}]`, '连接关闭', ev.code, ev.reason)
+
+    if (ev.code === 1002) {
+      console.error(`[${protocols}]`, '远程服务器拒绝连接，不再尝试重新创建连接，请手动刷新页面！')
+      return
+    }
+
     console.log(`[${protocols}]`, '将于 5 秒后尝试重新创建连接...')
+
     if (websocket.readyState !== WebSocket.OPEN || websocket.readyState !== WebSocket.CONNECTING) {
       window.setTimeout(() => createSocket(onMessageCallback, protocols), 5000)
     }
