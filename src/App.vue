7dfd5c2ad77bdd036c0b79e953e94c15d1fa4cab
hhui64 <907322015@qq.com>
2021-08-26 11:05:56 +0800

    feat(App): 新增全局配置

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 
 export default defineComponent({
   setup() {
-    return {}
+    return {
+      locale: zhCN
+    }
   }
 })
 </script>
