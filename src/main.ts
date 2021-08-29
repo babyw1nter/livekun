bf59e89e74101e8c4242c279585e61e2cee5b501
hhui64 <907322015@qq.com>
2021-08-30 04:57:59 +0800

    perf: 优化组件按需引用以减少打包的文件体积

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,29 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store, { key } from './store'
-import antd from 'ant-design-vue'
+import {
+  ConfigProvider,
+  Layout,
+  Button,
+  Input,
+  InputNumber,
+  Switch,
+  Slider,
+  Checkbox,
+  Divider,
+  Space,
+  Menu,
+  Tabs,
+  Form,
+  Alert,
+  Typography,
+  Avatar,
+  Image
+} from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
-import '@/assets/custom.less'
-import 'dayjs/locale/zh-cn'
 import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import '@/assets/custom.less'
 
 const app = createApp(App)
 
@@ -15,5 +33,21 @@ dayjs.locale('zh-cn')
 app
   .use(store, key)
   .use(router)
-  .use(antd)
+  .use(ConfigProvider)
+  .use(Layout)
+  .use(Button)
+  .use(Menu)
+  .use(Input)
+  .use(InputNumber)
+  .use(Switch)
+  .use(Slider)
+  .use(Checkbox)
+  .use(Divider)
+  .use(Space)
+  .use(Tabs)
+  .use(Form)
+  .use(Alert)
+  .use(Typography)
+  .use(Avatar)
+  .use(Image)
   .mount('#app')
