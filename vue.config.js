9c44804570d6635da668755ce07471571e20fac8
hhui64 <907322015@qq.com>
2021-09-03 20:15:56 +0800

    chore: 修改 webpack 相关构建配置

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,13 @@
 const webpackBundleAnalyzer = require('webpack-bundle-analyzer')
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
 
+const isProduction = process.env.NODE_ENV !== 'development'
+
 module.exports = {
+  productionSourceMap: !isProduction,
+  lintOnSave: false,
   chainWebpack: config => {
-    config.plugin('webpack-bundle-analyzer').use(webpackBundleAnalyzer.BundleAnalyzerPlugin)
+    isProduction && config.plugin('webpack-bundle-analyzer').use(webpackBundleAnalyzer.BundleAnalyzerPlugin)
     config.plugin('antd-dayjs-webpack-plugin').use(new AntdDayjsWebpackPlugin())
   }
 }
