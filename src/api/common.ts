1aedf7bb92cb4be0f80f1e3f9f50346e33208aed
hhui64 <907322015@qq.com>
2021-09-03 22:20:08 +0800

    fix: 修复当礼物价值低于第一档金额时风格样式异常的问题

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,7 +1,7 @@
 const getLevel = (money: number, moneyLevelArray: Array<number>): number => {
   for (let i = 0; i < moneyLevelArray.length; i++) {
     if (money >= moneyLevelArray[moneyLevelArray.length - 1]) return moneyLevelArray.length - 1
-    if (money < moneyLevelArray[i]) return i - 1
+    if (money < moneyLevelArray[i]) return i <= 0 ? 0 : i - 1
   }
   return 0
 }
