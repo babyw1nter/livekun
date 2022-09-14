adaabbae0bf1ebb3587656e6c71fef0a95620047
HUANGHU1 <907322015@qq.com>
2022-09-14 23:33:01 +0800

    chore: add mini-css-extract-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const webpackBundleAnalyzer = require('webpack-bundle-analyzer')
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProduction = process.env.NODE_ENV !== 'development'
 
@@ -10,5 +11,6 @@ module.exports = {
   chainWebpack: config => {
     // isProduction && config.plugin('webpack-bundle-analyzer').use(webpackBundleAnalyzer.BundleAnalyzerPlugin)
     config.plugin('antd-dayjs-webpack-plugin').use(new AntdDayjsWebpackPlugin())
+    // config.plugin('mini-css-extract-plugin').use(new MiniCssExtractPlugin())
   }
 }
