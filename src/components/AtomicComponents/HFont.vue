f282dd7d6d27dd052a9e7d7d79711a4924ad211d
hhui64 <907322015@qq.com>
2023-02-23 00:04:32 +0800

    perf: 优化文本中的 Emoji 样式

---
+++ src/components/AtomicComponents/HFont.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { replaceEmoji } from '@/api/common'
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  },
+  color: String,
+  fontSize: {
+    type: Number,
+    default: 18
+  }
+})
+
+const t = computed(() => replaceEmoji(props.text))
+</script>
+
+<template>
+  <span class="h-font" :style="{ color, fontSize: `${fontSize}px` }" v-html="t">
+  </span>
+</template>
