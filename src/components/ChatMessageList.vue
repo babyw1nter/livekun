767152a0b23f3a35719dbe9e9f73bdcfbfeafd47
hhui64 <907322015@qq.com>
2023-03-05 16:53:38 +0800

    feat: 新增聊天消息插件顶部的 Ticket 栏

--- src/components/ChatMessageList.vue
+++ src/components/ChatMessageList.vue
@@ -1,44 +1,49 @@
 <template>
-  <ul ref="dom" class="chat-message-list clearfix" :class="{ 'smooth-scroll': !isTooQuickly }">
-    <template v-for="item in chatMessageList" :key="item.key">
-      <li class="chat-list-message">
-        <ChatMessage
-          v-if="item.messageType === 'chat'"
-          :avatar-url="item.avatarUrl"
-          :nickname="item.nickname"
-          :message="item.message"
-          :admin="item.admin"
-          :guard="item.guard"
-          :badgeInfo="item.badgeInfo"
-          :custom-style="item.customStyle"
-          :font-size="fontSize"
-          :type="item.type"
-        >
-        </ChatMessage>
-      </li>
-      <li class="chat-list-gift" v-if="item.messageType === 'gift'">
-        <Paid
-          :type="item.type || `level-${getLevel(item?.money || 0, store.state.config.paid.level)}`"
-          :avatar-url="item.avatarUrl"
-          :nickname="item.nickname"
-          :money="item.money"
-          :gift-name="item.giftName"
-          :gift-count="item.giftCount"
-          :gift-image="item.giftImage"
-          :gift-icon="item.giftIcon"
-          :message="item.message"
-          :comment="item.comment"
-        >
-        </Paid>
-      </li>
-    </template>
-  </ul>
+  <div class="chat-message-panel" :class="{ 'show-ticket-panel': showTicketPanel }">
+    <TicketPanel ref="TicketPanelRef" :maximum="store.state.config.ticket.maximum"
+      :level="store.state.config.ticket.level" :duration="store.state.config.ticket.duration"
+      class="chat-message-ticket-panel mb-0" />
+    <ul ref="dom" class="chat-message-list clearfix mb-0" :class="{ 'smooth-scroll': !isTooQuickly }">
+      <template v-for="item in chatMessageList" :key="item.key">
+        <li class="chat-list-message" v-if="item.messageType === 'chat'">
+          <ChatMessage
+            :avatar-url="item.avatarUrl"
+            :nickname="item.nickname"
+            :message="item.message"
+            :admin="item.admin"
+            :guard="item.guard"
+            :badgeInfo="item.badgeInfo"
+            :custom-style="item.customStyle"
+            :font-size="fontSize"
+            :type="item.type"
+          >
+          </ChatMessage>
+        </li>
+        <li class="chat-list-gift" v-if="item.messageType === 'gift'">
+          <Paid
+            :type="item.type || `level-${getLevel(item?.money || 0, store.state.config.paid.level)}`"
+            :avatar-url="item.avatarUrl"
+            :nickname="item.nickname"
+            :money="item.money"
+            :gift-name="item.giftName"
+            :gift-count="item.giftCount"
+            :gift-image="item.giftImage"
+            :gift-icon="item.giftIcon"
+            :message="item.message"
+            :comment="item.comment"
+          >
+          </Paid>
+        </li>
+      </template>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from 'vuex'
 import { key } from '@/store'
 import { getLevel } from '@/api/common'
+import type TicketPanel from '@/components/TicketPanel.vue'
 
 interface ChatMessage {
   key: string
@@ -83,7 +88,11 @@ const props = defineProps({
   },
   fontSize: {
     type: Number,
-    default: 16
+    default: 18
+  },
+  showTicketPanel: {
+    type: Boolean,
+    default: true
   }
 })
 
@@ -91,6 +100,7 @@ const store = useStore(key)
 
 const dom = ref<HTMLElement>()
 const chatMessageList = reactive<ChatMessage[]>([])
+const TicketPanelRef = ref<InstanceType<typeof TicketPanel>>()
 
 watch(chatMessageList, () => {
   nextTick(() => {
@@ -115,17 +125,27 @@ const add = (chatMessage: ChatMessage) => {
   interval = nowTimestamp
 
   chatMessageList.push(chatMessage)
+
+  if (chatMessage.messageType === 'gift') {
+    const msg = chatMessage as Required<ChatMessage>
+    msg.type = ''
+    msg.message = ''
+    TicketPanelRef.value?.add({
+      ...msg,
+    })
+  }
 }
 
 const del = (key: string) => {
   const index = chatMessageList.findIndex(i => i.key === key)
-    if (index > -1) {
+  if (index > -1) {
     chatMessageList.splice(index, 1)
   }
 }
 
 const clear = () => {
   chatMessageList.splice(0, chatMessageList.length)
+  TicketPanelRef.value?.clear()
 }
 
 defineExpose({
@@ -136,28 +156,50 @@ defineExpose({
 </script>
 
 <style lang="less" scoped>
-.chat-message-list {
-  overflow-y: auto;
-  list-style: none;
+.chat-message-panel {
+  background-color: transparent;
   padding-left: 10px;
   padding-right: 10px;
   width: 100%;
   height: 100%;
 
-  &.smooth-scroll {
-    scroll-behavior: smooth;
+  .chat-message-ticket-panel {
+    display: none;
+    padding-left: 0;
+    padding-right: 0;
   }
 
-  &::-webkit-scrollbar {
-    width: 0 !important;
+  &.show-ticket-panel {
+    .chat-message-ticket-panel {
+      display: block;
+    }
+    .chat-message-list {
+      height: calc(100% - 55px);
+    }
   }
 
-  li.chat-list-message {
+  .chat-message-list {
+    padding-bottom: 10px;
+    overflow-y: auto;
     list-style: none;
-  }
+    width: 100%;
+    height: 100%;
 
-  li.chat-list-gift {
-    margin: 8px 1rem;
+    &.smooth-scroll {
+      scroll-behavior: smooth;
+    }
+
+    &::-webkit-scrollbar {
+      width: 0 !important;
+    }
+
+    li.chat-list-message {
+      list-style: none;
+    }
+
+    li.chat-list-gift {
+      margin: 8px 1rem;
+    }
   }
 }
 </style>
