c84268735f98338a0bc46f29257b2735af5e1601
hhui64 <907322015@qq.com>
2023-03-11 19:43:08 +0800

    refactor(ChatMessage): 重写插件配置页面

--- src/views/Home/Config/ChatMessageConfigPage.vue
+++ src/views/Home/Config/ChatMessageConfigPage.vue
@@ -1,111 +1,259 @@
 <template>
   <div class="config-chat-message options-panel">
-    <a-row type="flex">
-      <a-col flex="auto">
-        <a-space direction="vertical" :size="24" style="width: 100%">
-          <a-space direction="vertical">
-            <a-typography-text>文字大小</a-typography-text>
-            <a-typography-text type="secondary"> 控制聊天消息的文字大小 </a-typography-text>
-            <a-slider :min="18" :max="32"
-              v-model:value="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.style.fontSize"
-              style="width: 280px; margin-bottom: 3rem" :marks="{ 18: '小', 32: '大' }" />
-          </a-space>
+    <a-row :gutter="[16, 16]">
+      <a-col :sm="24" :lg="14" :xl="16">
+        <a-space direction="vertical" size="middle" style="width: 100%;">
+          <a-card>
+            <a-space direction="vertical" style="width: 100%;">
+              <a-typography-text strong>自定义样式</a-typography-text>
 
-          <a-space direction="vertical">
-            <a-typography-text><a-tag color="orange">开发中</a-tag>消息样式控制</a-typography-text>
-            <a-typography-text type="secondary"> 自定义消息的样式 </a-typography-text>
-            <a-space :size="10" style="margin-top: 1rem">
-              <a-checkbox :checked="false" disabled>粉丝牌</a-checkbox>
-              <a-checkbox :checked="false" disabled>守护图标</a-checkbox>
-              <a-checkbox :checked="false" disabled>房管图标</a-checkbox>
-              <a-checkbox :checked="false" disabled>主播图标</a-checkbox>
-              <a-checkbox :checked="false" disabled>贵族图标</a-checkbox>
-              <a-checkbox :checked="true" disabled>昵称特殊颜色</a-checkbox>
+              <a-form :label-align="'right'" :label-col="labelCol" :wrapper-col="wrapperCol" style="max-width: none;">
+                <a-row :gutter="24" style="margin: 0;">
+                  <a-divider orientation="left">文本样式</a-divider>
+
+                  <a-col :xs="24" :sm="24" :md="12" :lg="24" :xl="12">
+                    <a-form-item label="文字大小">
+                      <a-select ref="select"
+                        v-model:value="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.fontSize">
+                        <a-select-option :value="i + 17" v-for="i in 15" :key="i + 987">{{ i + 17 }}px</a-select-option>
+                      </a-select>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="24" :md="12" :lg="24" :xl="12">
+                    <a-form-item label="文字字体">
+                      <a-select ref="select" :value="'default'">
+                        <a-select-option value="default">默认</a-select-option>
+                      </a-select>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-divider orientation="left">普通用户</a-divider>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.normal.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.normal.message">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-divider orientation="left">守护、舰长</a-divider>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="月守、舰长昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv1.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+
+                    <a-form-item label="年守、提督昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv2.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+
+                    <a-form-item label="总督昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv3.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="月守、舰长消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv1.message">
+                      </ColorPicker>
+                    </a-form-item>
+
+                    <a-form-item label="年守、提督消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv2.message">
+                      </ColorPicker>
+                    </a-form-item>
+
+                    <a-form-item label="总督消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.guard.lv3.message">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-divider orientation="left">房管</a-divider>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.admin.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.admin.message">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-divider orientation="left">主播</a-divider>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="昵称颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.anchor.nickname">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="消息颜色">
+                      <ColorPicker format="hex" disable-alpha disable-history
+                        v-model:pure-color="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.customStyle.color.anchor.message">
+                      </ColorPicker>
+                    </a-form-item>
+                  </a-col>
+
+                </a-row>
+              </a-form>
             </a-space>
-          </a-space>
+          </a-card>
 
-          <a-space direction="vertical">
-            <a-typography-text><a-tag color="orange">开发中</a-tag>消息显示控制</a-typography-text>
-            <a-typography-text type="secondary"> 控制显示哪些聊天消息 </a-typography-text>
-            <a-space :size="10" style="margin-top: 1rem">
-              <a-checkbox v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.show.join"
-                disabled>进入直播间</a-checkbox>
-              <a-checkbox v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.show.follow"
-                disabled>用户关注</a-checkbox>
-              <a-checkbox v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.show.gift"
-                disabled>赠送礼物</a-checkbox>
+          <a-card>
+            <a-space direction="vertical" style="width: 100%;">
+              <a-typography-text strong>业务、身份图标显示 <a-tag color="orange">未完成</a-tag></a-typography-text>
+
+              <a-divider />
+
+              <a-form :label-align="'right'" :label-col="labelCol" :wrapper-col="wrapperCol" style="max-width: none;">
+                <a-row :gutter="24" style="margin: 0;">
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+
+                    <a-form-item label="粉丝徽章">
+                      <a-switch :checked="false" checked-children="开" un-checked-children="关" disabled />
+                    </a-form-item>
+
+                    <a-form-item label="守护、舰长图标">
+                      <a-switch :checked="false" checked-children="开" un-checked-children="关" disabled />
+                    </a-form-item>
+
+                    <a-form-item label="贵族图标">
+                      <a-switch :checked="false" checked-children="开" un-checked-children="关" disabled />
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="房管图标">
+                      <a-switch :checked="true" checked-children="开" un-checked-children="关" disabled />
+                    </a-form-item>
+
+                    <a-form-item label="主播图标">
+                      <a-switch :checked="false" checked-children="开" un-checked-children="关" disabled />
+                    </a-form-item>
+
+                  </a-col>
+                </a-row>
+              </a-form>
             </a-space>
-          </a-space>
+          </a-card>
 
-          <a-space direction="vertical">
+          <a-card>
+            <a-space direction="vertical" style="width: 100%;">
+              <a-typography-text strong>额外内容显示</a-typography-text>
+
+              <a-divider />
+
+              <a-form :label-align="'right'" :label-col="labelCol" :wrapper-col="wrapperCol" style="max-width: none;">
+                <a-row :gutter="24" style="margin: 0;">
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="Ticket 价签栏">
+                      <a-switch v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.type.ticket"
+                        checked-children="开" un-checked-children="关" />
+                    </a-form-item>
+                  </a-col>
+
+                  <a-col :xs="24" :sm="12" :lg="24" :xl="12">
+                    <a-form-item label="Paid 礼物卡片">
+                      <a-switch v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.type.paid"
+                        checked-children="开" un-checked-children="关" />
+                    </a-form-item>
+                  </a-col>
+                </a-row>
+              </a-form>
+            </a-space>
+          </a-card>
+
+          <a-card>
+            <a-space direction="vertical" style="width: 100%;">
+              <a-typography-text strong>事件消息显示 <a-tag color="orange">未完成</a-tag></a-typography-text>
+
+              <a-divider />
+
+              <a-form :label-align="'right'" :label-col="labelCol" :wrapper-col="wrapperCol" style="max-width: none;">
+                <a-row :gutter="24" style="margin: 0;">
+                  <a-col :xs="24">
+                    <a-form-item label="事件类型">
+                      <a-checkbox-group>
+                        <a-checkbox
+                          v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.event.join"
+                          disabled>进入直播间</a-checkbox>
+                        <a-checkbox
+                          v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.event.follow"
+                          disabled>用户关注</a-checkbox>
+                        <a-checkbox
+                          v-model:checked="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.event.gift"
+                          disabled>赠送礼物</a-checkbox>
+                      </a-checkbox-group>
+                    </a-form-item>
+                  </a-col>
+                </a-row>
+              </a-form>
+            </a-space>
+          </a-card>
+
+          <a-card>
+            <a-space direction="vertical">
+              <a-typography-text strong>插件控制</a-typography-text>
+              <!-- <a-typography-text type="secondary"> 控制在 OBS 中添加的插件 </a-typography-text> -->
+              <a-space :size="10" style="margin-top: 1rem">
+                <a-button @click="sendMock">发送模拟数据至插件</a-button>
+                <a-button @click="clear">清空插件数据</a-button>
+              </a-space>
+            </a-space>
+          </a-card>
+
+          <a-card>
             <a-space :size="10">
-              <a-button @click="sendMock">发送模拟数据至插件</a-button>
-              <a-button @click="clear">清空插件数据</a-button>
+              <a-button type="primary" @click="chatMessagePluginConfig.save">保存设置</a-button>
+              <a-button danger @click="chatMessagePluginConfig.reset">恢复默认</a-button>
             </a-space>
-          </a-space>
-
-          <!-- <a-space direction="vertical">
-        <a-typography-text>黑名单用户设置</a-typography-text>
-        <a-typography-text type="secondary"> 过滤指定用户的聊天消息 </a-typography-text>
-        <a-space :size="10" style="margin-top: 1rem">
-          <a-button type="primary" @click="showBlacklistModel" ghost>编辑黑名单</a-button>
-          <span>黑名单用户数: {{ chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.blacklist.length }}</span>
-        </a-space>
-      </a-space> -->
-          <a-space :size="10">
-            <a-button type="primary" @click="chatMessagePluginConfig.save">保存设置</a-button>
-            <a-button danger @click="chatMessagePluginConfig.reset">恢复默认</a-button>
-          </a-space>
+          </a-card>
         </a-space>
       </a-col>
 
-      <a-col flex="370px">
-        <div class="preview-wrapper">
-          <ChatMessageList ref="ChatMessageListRef" class="preview-chat-message-list"
-            :font-size="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.style.fontSize"
-            :ticket-config="ticketPluginConfig.reactivityPluginConfig.pluginConfig"
-            :paid-config="paidPluginConfig.reactivityPluginConfig.pluginConfig" />
-        </div>
-        <a-space :size="5">
-          <a-switch v-model:checked="autoPreview" checked-children="开" un-checked-children="关" @change="autoPreviewChange"
-            style="vertical-align: bottom;">
-          </a-switch>
-          <a-typography-text>自动预览</a-typography-text>
-        </a-space>
-        <a-typography-link :href="url" :copyable="{ text: url }" target="_blank" style="float: right;">
-          插件链接
-        </a-typography-link>
+      <a-col :sm="24" :lg="10" :xl="8">
+        <a-affix :offset-top="16" style="width: 100%;">
+          <PluginPreviewWrapper class="plugin-chat-message-preview" :plugin-name="PluginNames.PLUGIN_CHAT_MESSAGE"
+            v-model:auto-preview="autoPreview" @on-auto-preview-switch-change="autoPreviewSwitchChange">
+            <ChatMessageList ref="ChatMessageListRef" class="preview-chat-message-list"
+              :chat-message-config="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig"
+              :ticket-config="ticketPluginConfig.reactivityPluginConfig.pluginConfig"
+              :paid-config="paidPluginConfig.reactivityPluginConfig.pluginConfig" />
+          </PluginPreviewWrapper>
+        </a-affix>
       </a-col>
+
     </a-row>
-
-    <!-- <a-modal v-model:visible="showBlacklistModelStatus" title="编辑黑名单" @ok="showBlacklistModel">
-      <template #footer>
-        <a-button key="submit" type="primary" @click="showBlacklistModel">完成</a-button>
-      </template>
-
-      <a-alert message="添加或移除用户后别忘了保存设置哦~" type="info" show-icon />
-
-      <a-space :size="10" style="margin-top: 20px">
-        <a-input v-model:value="blacklistUserCCID" placeholder="请输入用户CCID" />
-        <a-input v-model:value="blacklistUserNote" placeholder="备注信息..." />
-        <a-button type="primary" @click="addBlacklistUser" ghost>添加用户</a-button>
-      </a-space>
-
-      <a-table
-        style="margin-top: 10px"
-        size="middle"
-        :columns="blacklistColumns"
-        :data-source="chatMessagePluginConfig.reactivityPluginConfig.pluginConfig.blacklist"
-        :pagination="{ hideOnSinglePage: true }"
-        :locale="{ emptyText: '暂时还没有用户捏~' }"
-      >
-        <template #bodyCell="{ column, record }">
-          <template v-if="column.key === 'operation'">
-            <a-button danger size="small" @click="delBlacklistUser(record.ccid)">移除</a-button>
-          </template>
-        </template>
-      </a-table>
-    </a-modal> -->
   </div>
 </template>
 
@@ -117,6 +265,7 @@ import type ChatMessageList from '@/components/ChatMessageList.vue'
 import http from '@/api/http'
 import { getRandomChatMessage } from '@/api/mock'
 import { useChatMessagePluginConfig } from '@/api/config'
+import { PluginNames } from '@/api/plugins'
 
 const store = useStore(key)
 const ChatMessageListRef = ref<InstanceType<typeof ChatMessageList>>()
@@ -125,7 +274,7 @@ let { chatMessagePluginConfig, ticketPluginConfig, paidPluginConfig } = useChatM
 
 const autoPreviewTimer = ref(0)
 const autoPreview = ref(true)
-const autoPreviewChange = () => {
+const autoPreviewSwitchChange = () => {
   ChatMessageListRef.value?.add({
     ...getRandomChatMessage()
   })
@@ -140,9 +289,7 @@ const autoPreviewChange = () => {
   }
 }
 
-onMounted(() => autoPreviewChange())
-
-const url = computed(() => `${window.location.origin}/#/plugins/chat-message?uuid=${store.state.auth.uuid}`)
+onMounted(() => autoPreviewSwitchChange())
 
 const sendMock = () => {
   http
@@ -165,19 +312,33 @@ const clear = () => {
       message.error(reason.toString())
     })
 }
+
+const labelCol = {
+  // sm: { span: 4 },
+  // lg: { span: 24 },
+  style: {
+    width: '150px'
+  }
+}
+
+const wrapperCol = {
+  // sm: { span: 14 },
+  // lg: { span: 24, offset: 24 },
+  // style: {
+  //   width: '150px'
+  // }
+}
 </script>
 
 <style lang="less" scoped>
 .config-chat-message {
+  .plugin-chat-message-preview {
+    height: calc(100vh - 150px);
+  }
+
   .preview-chat-message-list {
-    // position: absolute;
     width: 100%;
     height: 100%;
-    // max-height: 456px;
-    // top: 20px;
-    // left: 20px;
-    // border: 1px dashed #ccc;
-    // resize: both;
     overflow: hidden;
   }
 }
