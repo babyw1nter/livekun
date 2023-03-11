4899a8e32c4de9ca952cfb5e8d800fa5b7b566b9
hhui64 <907322015@qq.com>
2023-03-11 19:33:46 +0800

    chore: 添加 `Vue3ColorPicker` 插件

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store, { key } from './store'
 import dayjs from 'dayjs'
+import Vue3ColorPicker from 'vue3-colorpicker'
+import 'vue3-colorpicker/style.css'
 import 'dayjs/locale/zh-cn'
 import 'ant-design-vue/es/message/style/css'
 import 'ant-design-vue/es/notification/style/css'
@@ -11,6 +13,8 @@ import '@/assets/custom.less'
 const app = createApp(App)
 
 dayjs.locale('zh-cn')
+
+app.use(Vue3ColorPicker)
 
 app.use(store, key).use(router)
 
