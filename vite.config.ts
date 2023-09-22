9c910654b577a07bcab8b00d5cda96ae4cd7b3d9
hhui64 <907322015@qq.com>
2023-09-23 05:53:57 +0800

    ci: build for github

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -9,46 +9,50 @@ import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-    strictPort: true
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      strictPort: true
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
       }
-    }
-  },
-  plugins: [
-    vue(),
-    eslint({
-      cache: true
-    }),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: 'src/typings/auto-imports.d.ts'
-    }),
-    Components({
-      dts: 'src/typings/components.d.ts',
-      resolvers: [
-        AntDesignVueResolver({ importStyle: 'less', resolveIcons: true }),
-        IconsResolver(),
-        (componentName) => {
-          return componentName === 'ColorPicker' ? { name: 'ColorPicker', from: 'vue3-colorpicker' } : undefined
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true
         }
-      ],
-      allowOverrides: true
-    }),
-    Icons({
-      compiler: 'vue3'
-    })
-  ]
+      }
+    },
+    plugins: [
+      vue(),
+      eslint({
+        cache: true
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        dts: 'src/typings/auto-imports.d.ts'
+      }),
+      Components({
+        dts: 'src/typings/components.d.ts',
+        resolvers: [
+          AntDesignVueResolver({ importStyle: 'less', resolveIcons: true }),
+          IconsResolver(),
+          (componentName) => {
+            return componentName === 'ColorPicker' ? { name: 'ColorPicker', from: 'vue3-colorpicker' } : undefined
+          }
+        ],
+        allowOverrides: true
+      }),
+      Icons({
+        compiler: 'vue3'
+      })
+    ]
+  }
 })
