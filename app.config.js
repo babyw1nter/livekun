cd0d0ba5a0d74999f7895a11656d2faf27a76699
hhui64 <907322015@qq.com>
2023-09-22 21:15:06 +0000

    Deploying to gh-pages from  @ 0c14391dfedcc35a09fd89e74f2b90c7d1714be2 🚀

---
+++ app.config.js
@@ -0,0 +1,11 @@
+const appConfig = {
+  copyright: '© 2023 hhui64. Made for Linger, Customized.',
+  icp: {
+    beian: '桂ICP备15007582号-4',
+    url: 'https://beian.miit.gov.cn/'
+  },
+  gongan: {
+    beian: '桂公网安备45010302002861号',
+    url: 'https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=45010302002861'
+  }
+}
