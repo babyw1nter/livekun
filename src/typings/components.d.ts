aa6863e2bfbee19b4824fd059ec5e56512bf198c
hhui64 <907322015@qq.com>
2023-09-23 08:57:37 +0800

    chore: typings

--- src/typings/components.d.ts
+++ src/typings/components.d.ts
@@ -30,6 +30,7 @@ declare module 'vue' {
     AMenu: typeof import('ant-design-vue/es')['Menu']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
     AModal: typeof import('ant-design-vue/es')['Modal']
+    APopconfirm: typeof import('ant-design-vue/es')['Popconfirm']
     AppstoreOutlined: typeof import('@ant-design/icons-vue')['AppstoreOutlined']
     AResult: typeof import('ant-design-vue/es')['Result']
     ARow: typeof import('ant-design-vue/es')['Row']
